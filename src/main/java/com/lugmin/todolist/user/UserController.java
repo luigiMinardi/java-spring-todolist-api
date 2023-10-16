42d15cbaee6b819df1907d4867bda88aa9fca3de
Luigi <lugminsinlag@gmail.com>
2023-10-16 13:33:55 +0200

    feat: User post endpoint and password hashing.

--- src/main/java/com/lugmin/todolist/user/UserController.java
+++ src/main/java/com/lugmin/todolist/user/UserController.java
@@ -1,6 +1,11 @@
 package com.lugmin.todolist.user;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+   
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);       
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            return ResponseEntity.status(400).body("User already exists.");
+        }
+
+        var passwordHash = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHash);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(201).body("User created.");
     }
 }
