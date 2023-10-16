26c7a7bc5599f1bd13f9e62a60eeb164beb26806
Luigi <lugminsinlag@gmail.com>
2023-10-16 21:15:39 +0200

    feat: Task list endpoint and date validation on task create.

--- src/main/java/com/lugmin/todolist/task/TaskController.java
+++ src/main/java/com/lugmin/todolist/task/TaskController.java
@@ -1,8 +1,12 @@
 package com.lugmin.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +22,22 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         taskModel.setIdUser((UUID) request.getAttribute("idUser"));
-        var task = this.taskRepository.save(taskModel);
 
-        return task;
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || taskModel.getEndAt().isBefore(taskModel.getStartAt())) {
+            return ResponseEntity.status(400).body("You can't create a task in the past nor set it to end before the starting time.");
+        } else {
+            var task = this.taskRepository.save(taskModel);
+
+            return ResponseEntity.status(200).body(task);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<TaskModel>> list(HttpServletRequest request) {
+        var tasks = this.taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
+        return ResponseEntity.status(200).body(tasks);
     }
 }
